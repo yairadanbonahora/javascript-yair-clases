42ac93ce4a5f93617659a39510e4e257705dcc56
yairadanbonahora <bonahorayair@gmail.com>
2021-11-17 19:56:26 -0300

    primera clase

---
+++ main.js
@@ -0,0 +1,18 @@
+//PRIMERA CLASE
+let nombre = prompt ("Ingrese su nombre");
+alert ("Hola," + " " + nombre + "!");
+
+let numero = prompt ("Ingrese monto a comprar USD");
+let monto = parseInt (numero);
+
+let importeTotal = monto + 20;
+alert ("Usted debe pagar:" + " " + importeTotal);
+
+let usuario = prompt ("Ingrese su nombre");
+let usuarioApellido = prompt ("Ingrese su apellido");
+let nombreCompleto = usuario + " " + usuarioApellido;
+alert (nombreCompleto);
+
+console.log (nombre)
+console.log (importeTotal)
+console.log (nombreCompleto)
