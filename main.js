e4e5338e7bee1eff5b2c1a33dae5df971e7d6960
yairadanbonahora <bonahorayair@gmail.com>
2021-11-24 17:27:56 -0300

    segunda clase

--- main.js
+++ main.js
@@ -1,18 +1,43 @@
 //PRIMERA CLASE
-let nombre = prompt ("Ingrese su nombre");
-alert ("Hola," + " " + nombre + "!");
+// let nombre = prompt ("Ingrese su nombre");
+// alert ("Hola," + " " + nombre + "!");
 
-let numero = prompt ("Ingrese monto a comprar USD");
-let monto = parseInt (numero);
+// let numero = prompt ("Ingrese monto a comprar USD");
+// let monto = parseInt (numero);
 
-let importeTotal = monto + 20;
-alert ("Usted debe pagar:" + " " + importeTotal);
+// let importeTotal = monto + 20;
+// alert ("Usted debe pagar:" + " " + importeTotal);
 
-let usuario = prompt ("Ingrese su nombre");
-let usuarioApellido = prompt ("Ingrese su apellido");
-let nombreCompleto = usuario + " " + usuarioApellido;
-alert (nombreCompleto);
+// let usuario = prompt ("Ingrese su nombre");
+// let usuarioApellido = prompt ("Ingrese su apellido");
+// let nombreCompleto = usuario + " " + usuarioApellido;
+// alert (nombreCompleto);
 
-console.log (nombre)
-console.log (importeTotal)
-console.log (nombreCompleto)
+// console.log (nombre)
+// console.log (importeTotal)
+// console.log (nombreCompleto)
+
+//SEGUNDA CLASE
+let nombreIngresado = prompt ("Ingrese su nombre");
+let nombre = nombreIngresado;
+let apellidoIngresado = prompt ("Ingrese su apellido");
+let apellido = apellidoIngresado; 
+
+//para verificar que escriba un nombre
+if (nombreIngresado == "" || apellidoIngresado == "") {
+    alert ("Por favor ingrese su nombre completo");
+} else if (nombre == nombreIngresado && apellido == apellidoIngresado){
+    alert ("Bienvenido" + " " + nombre + " " + apellido + "!");
+}
+
+let edad = prompt ("Ingrese su edad");
+let edadParseada = parseInt (edad);
+
+//para saber si es menor/mayor de edad una vez ingresada la edad
+if (edadParseada < 18) {
+    alert ("Usted es menor de edad")
+} else if (edadParseada > 18) {
+    alert ("Usted es mayor de edad");
+} else if (edad == "") {
+    alert ("Por favor ingrese su edad"); 
+}
