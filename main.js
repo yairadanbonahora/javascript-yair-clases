7b8cbe5a89a2bb34e610a61ccb9cc76a93adbf4b
yairadanbonahora <bonahorayair@gmail.com>
2021-11-24 17:37:53 -0300

    tercera clase

--- main.js
+++ main.js
@@ -18,26 +18,70 @@
 // console.log (nombreCompleto)
 
 //SEGUNDA CLASE
-let nombreIngresado = prompt ("Ingrese su nombre");
-let nombre = nombreIngresado;
-let apellidoIngresado = prompt ("Ingrese su apellido");
-let apellido = apellidoIngresado; 
+// let nombreIngresado = prompt ("Ingrese su nombre");
+// let nombre = nombreIngresado;
+// let apellidoIngresado = prompt ("Ingrese su apellido");
+// let apellido = apellidoIngresado; 
 
-//para verificar que escriba un nombre
-if (nombreIngresado == "" || apellidoIngresado == "") {
-    alert ("Por favor ingrese su nombre completo");
-} else if (nombre == nombreIngresado && apellido == apellidoIngresado){
-    alert ("Bienvenido" + " " + nombre + " " + apellido + "!");
+// para verificar que escriba un nombre
+// if (nombreIngresado == "" || apellidoIngresado == "") {
+//     alert ("Por favor ingrese su nombre completo");
+// } else if (nombre == nombreIngresado && apellido == apellidoIngresado){
+//     alert ("Bienvenido" + " " + nombre + " " + apellido + "!");
+// }
+
+// let edad = prompt ("Ingrese su edad");
+// let edadParseada = parseInt (edad);
+
+// para saber si es menor/mayor de edad una vez ingresada la edad
+// if (edadParseada < 18) {
+//     alert ("Usted es menor de edad")
+// } else if (edadParseada > 18) {
+//     alert ("Usted es mayor de edad");
+// } else if (edad == "") {
+//     alert ("Por favor ingrese su edad"); 
+// }
+
+
+//TERCER CLASE
+let autor = prompt ("Ingrese nombre de autor");
+
+//buscador de autores y su sección correspondiente
+while (autor != "ESC") {
+    switch (autor) {
+        case "O´Donnell":
+            alert ("Está en la sección ´Política´");
+            break; 
+            
+        case "Foucault":
+            alert ("Está en la sección ´Filosofía´");
+            break;
+        
+        case "Raymond Aron":
+            alert ("Están en la sección ´Historia´");
+            break;
+
+        default: 
+            alert ("Ese autor no se encuentra disponible");
+    } 
+
+    autor = prompt ("Ingrese nombre de autor");
+    console.log (autor); 
 }
 
-let edad = prompt ("Ingrese su edad");
-let edadParseada = parseInt (edad);
-
-//para saber si es menor/mayor de edad una vez ingresada la edad
-if (edadParseada < 18) {
-    alert ("Usted es menor de edad")
-} else if (edadParseada > 18) {
-    alert ("Usted es mayor de edad");
-} else if (edad == "") {
-    alert ("Por favor ingrese su edad"); 
+//para otorgar turnos
+for (let turno = 1; turno <= 11; turno ++) {
+    if (turno == 11) {
+        alert ("Disculpe, el cupo está completo");
+        break;
+    }
+    let ingresarNombre = prompt ("Ingresar nombre");
+    console.log ("Turno N" + turno + " Nombre: " + ingresarNombre );
 }
+
+//era para probar el do jaja
+let colores = prompt ("Ingrese un color");
+do {
+    console.log (colores);
+    colores = prompt ("Ingrese otro color");
+} while (colores != "negro")
